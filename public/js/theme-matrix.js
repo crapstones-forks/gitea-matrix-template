dc3e3faa66ff33a41b583e835afa0f286ed434c6
crapStone <crapstone01@gmail.com>
2022-03-17 01:19:42 +0100

    basic performance optimizations and blackening before each new character

--- public/js/theme-matrix.js
+++ public/js/theme-matrix.js
@@ -1,35 +1,36 @@
 // Matrix canvas
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
-// Get client offsets
-var my_height =
+const c = document.getElementById("c");
+const ctx = c.getContext("2d");
+
+// making the canvas full screen
+c.height =
   "innerHeight" in window
     ? window.innerHeight
     : document.documentElement.offsetHeight;
-var my_width =
+c.width =
   "innerWidth" in window
     ? window.innerWidth
     : document.documentElement.offsetWidth;
 
-// making the canvas full screen
-c.height = my_height;
-c.width = my_width;
-
 // latin characters - taken from the unicode charset
-var matrix =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
 // converting the string into an array of single characters
-matrix = matrix.split("");
+const matrix =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}".split(
+    ""
+  );
 
-var font_size = 10;
-var columns = c.width / font_size; // number of columns for the rain
+const font_size = 10;
 // an array of drops - one per column
-var drops = [];
+const drops = [];
 // x below is the x coordinate
 // 1 = y co-ordinate of the drop(same for every drop initially)
-for (var x = 0; x < columns; x++) {
+for (let x = 0; x < c.width / font_size; x++) {
   drops[x] = 1;
 }
+
+// make the canvas black from the beginning
+ctx.fillStyle = "#000000";
+ctx.fillRect(0, 0, c.width, c.height);
 
 // drawing the characters
 function draw() {
@@ -38,23 +39,31 @@ function draw() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
   ctx.fillRect(0, 0, c.width, c.height);
 
-  ctx.fillStyle = "#5AA509"; //  Gitea green
   ctx.font = font_size + "px arial";
 
   // looping over drops
-  for (var i = 0; i < drops.length; i++) {
-    // a random chinese character to print
-    var text = matrix[Math.floor(Math.random() * matrix.length)];
-    ctx.fillText(text, i * font_size, drops[i] * font_size);
+  for (let i = 0; i < drops.length; i++) {
+    // only use the random function once to save cpu
+    const rand = Math.random();
 
-    // sending the drop back to the top randomly after it has crossed the screen
-    // adding a randomness to the reset to make the drops scattered on the Y axis
-    if (drops[i] * font_size > c.height && Math.random() > 0.975) {
-      drops[i] = 0;
-    }
+    // reset the background so that the characters don't blend into each other
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(i * font_size, (drops[i] - 1) * font_size, 10, 10);
+
+    // a random character to print
+    const text = matrix[Math.floor(rand * matrix.length)];
+    //  Gitea green
+    ctx.fillStyle = "#5AA509";
+    ctx.fillText(text, i * font_size, drops[i] * font_size);
 
     // incrementing Y coordinate
     drops[i]++;
+
+    // sending the drop back to the top randomly after it has crossed the screen
+    // adding a randomness to the reset to make the drops scattered on the Y axis
+    if (drops[i] * font_size > c.height && rand > 0.975) {
+      drops[i] = 1;
+    }
   }
 }
 
